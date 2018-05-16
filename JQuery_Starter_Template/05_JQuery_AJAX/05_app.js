b1825a5f32f53f504f07aeec4a20e19c5adedd4a
thenaveensaggam <thenaveensaggam@gmail.com>
2018-05-16 12:35:49 +0530

    add

--- JQuery_Starter_Template/05_JQuery_AJAX/05_app.js
+++ JQuery_Starter_Template/05_JQuery_AJAX/05_app.js
@@ -1,26 +1,12 @@
-$('#load-form-button').click(function () {
-    $('#card-body').load('form.html');
-});
-
-$('#load-images-btn').click(function () {
-    $.get('db.json', function (data) {
-        var image1 = data.contacts[0].picture.large;
-        var image1_name = data.contacts[0].name.first + " " + data.contacts[0].name.last;
-        $('#image1').attr('src',image1);
-        $('#image1_name').text(image1_name);
-
-        var image2 = data.contacts[1].picture.large;
-        var image2_name = data.contacts[1].name.first + " " + data.contacts[0].name.last;
-        $('#image2').attr('src',image2);
-        $('#image2_name').text(image2_name);
-
-        var image3 = data.contacts[2].picture.large;
-        $('#image3').attr('src',image3);
-
-        var image4 = data.contacts[3].picture.large;
-        $('#image4').attr('src',image4);
-
-        var image5 = data.contacts[4].picture.large;
-        $('#image5').attr('src',image5);
-    });
-});
+Used for Text formatting
+------------------------
+<b> - Bold text
+<strong> - Important text
+<i> - Italic text
+<em> - Emphasized text
+<mark> - Marked text
+<small> - Small text
+<del> - Deleted text
+<ins> - Inserted text
+<sub> - Subscript text
+<sup> - Superscript text
