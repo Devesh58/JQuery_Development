9965879e6535ad36a113a9123189d1e96fe84db8
thenaveensaggam <thenaveensaggam@gmail.com>
2018-05-21 21:52:38 +0530

    deleted

--- JQuery_Completed_Template/02_JQuery_Forms_Manual/02_app.js
+++
@@ -1,5 +0,0 @@
-$('#submit-button').click(function() {
-    $('#user_text').text($('#username').val());
-    $('#password_text').text($('#password').val());
-    $('#email_text').text($('#email').val());
-});
